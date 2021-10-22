965ff9d4ae167b72a801e94bf480ac3d90242c5b
Hendry Setiawan <hue.huendry@gmail.com>
2021-10-22 15:42:03 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "assets/scss/style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      salam brother
+    </div>
+  );
+}
+
+export default App;
